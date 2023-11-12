17bc6c6bc57a7541f46da58e4df7cc93b68bf3a1
faustaleonardo <faustaleonardo11@gmail.com>
2023-11-12 14:35:05 +0700

    backoff
    Signed-off-by: faustaleonardo <faustaleonardo11@gmail.com>

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -1,13 +1,10 @@
 const chat = document.getElementById("chat");
 const msgs = document.getElementById("msgs");
 
-// let's store all current messages here
 let allChat = [];
 
-// the interval to poll at in milliseconds
 const INTERVAL = 3000;
 
-// a submit listener on the form in the HTML
 chat.addEventListener("submit", function (e) {
   e.preventDefault();
   postNewMsg(chat.elements.user.value, chat.elements.text.value);
@@ -20,53 +17,54 @@ async function postNewMsg(user, text) {
     text,
   };
 
-  const body = JSON.stringify(data);
+  // request options
   const options = {
     method: "POST",
-    body,
+    body: JSON.stringify(data),
     headers: {
       "Content-Type": "application/json",
     },
   };
 
+  // send POST request
   await fetch("/poll", options);
 }
 
 async function getNewMsgs() {
-  let result = {};
   try {
-    const response = await fetch("/poll");
-    result = await response.json();
-  } catch (error) {
-    console.error("Error:", error);
-  }
+    const res = await fetch("/poll");
+    const json = await res.json();
 
-  allChat = result.msg;
-  render();
-  setTimeout(getNewMsgs, INTERVAL);
-}
+    if (res.status >= 400) {
+      throw new Error("request did not succeed: " + res.status);
+    }
 
-let timeToMakeNextRequest = 0;
-async function requestAnimationFrameTimer(time) {
-  if (timeToMakeNextRequest <= time) {
-    await getNewMsgs();
-    timeToMakeNextRequest = time + INTERVAL;
+    allChat = json.msg;
+    render();
+    failedTries = 0;
+  } catch (e) {
+    // back off
+    failedTries++;
   }
-  requestAnimationFrame(requestAnimationFrameTimer);
 }
 
 function render() {
-  // as long as allChat is holding all current messages, this will render them
-  // into the ui. yes, it's inefficent. yes, it's fine for this example
-  const html = allChat.map(({ user, text, time, id }) =>
-    template(user, text, time, id)
-  );
+  const html = allChat.map(({ user, text }) => template(user, text));
   msgs.innerHTML = html.join("\n");
 }
 
-// given a user and a msg, it returns an HTML string to render to the UI
 const template = (user, msg) =>
   `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
 
-// make the first request
-requestAnimationFrame(requestAnimationFrameTimer);
+const BACKOFF = 5000;
+let timeToMakeNextRequest = 0;
+let failedTries = 0;
+async function rafTimer(time) {
+  if (timeToMakeNextRequest <= time) {
+    await getNewMsgs();
+    timeToMakeNextRequest = time + INTERVAL + failedTries * BACKOFF;
+  }
+  requestAnimationFrame(rafTimer);
+}
+
+requestAnimationFrame(rafTimer);
