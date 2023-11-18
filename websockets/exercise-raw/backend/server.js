244ef6288e4fabb11bbf5d5bc38cae8e4413f84c
faustaleonardo <faustaleonardo11@gmail.com>
2023-11-18 16:42:45 +0700

    websocket
    Signed-off-by: faustaleonardo <faustaleonardo11@gmail.com>

--- websockets/exercise-raw/backend/server.js
+++ websockets/exercise-raw/backend/server.js
@@ -24,6 +24,48 @@ const server = http.createServer((request, response) => {
   });
 });
 
+server.on("upgrade", (request, socket) => {
+  if (request.headers["upgrade"] !== "websocket") {
+    socket.end("HTTP/1.1 400 Bad Request");
+    return;
+  }
+
+  const acceptKey = request.headers["sec-websocket-key"];
+  const acceptValue = generateAcceptValue(acceptKey);
+  const headers = [
+    "HTTP/1.1 101 Web Socket Protocol Handshake",
+    "Upgrade: WebSocket",
+    "Connection: Upgrade",
+    `Sec-WebSocket-Accept: ${acceptValue}`,
+    `Sec-WebSocket-Protocol: json`,
+    "\r\n", // telling websocket that we're done with the headers
+  ];
+
+  socket.write(headers.join("\r\n"));
+  socket.write(objToResponse({ msg: getMsgs() }));
+  connections.push(socket);
+
+  // receive message from client
+  socket.on("data", (buffer) => {
+    const { user, text } = parseMessage(buffer);
+    msg.push({
+      user,
+      text,
+      time: Date.now(),
+    });
+
+    connections.forEach((s) => {
+      s.write(objToResponse({ msg: getMsgs() }));
+    });
+  });
+
+  socket.on("close", () => {
+    connections = connections.filter((s) => s !== socket);
+  });
+
+  console.log("upgrade requested!");
+});
+
 /*
  *
  * your code goes here
