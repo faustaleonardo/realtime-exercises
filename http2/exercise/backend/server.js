8d0db11f7f941d9ad940aa9b19eb5e6a9ef7545d
faustaleonardo <faustaleonardo11@gmail.com>
2023-11-18 13:21:02 +0700

    http2
    Signed-off-by: faustaleonardo <faustaleonardo11@gmail.com>

--- http2/exercise/backend/server.js
+++ http2/exercise/backend/server.js
@@ -35,6 +35,26 @@ const server = http2.createSecureServer({
  *
  */
 
+server.on("stream", (stream, headers) => {
+  const path = headers[":path"];
+  const method = headers[":method"];
+
+  if (path === "/msgs" && method === "GET") {
+    stream.respond({
+      ":status": 200,
+      "content-type": "text/plain; charset=utf-8",
+    });
+
+    stream.write(JSON.stringify({ msgs: getMsgs() }));
+    connections.push(stream);
+
+    stream.on("close", () => {
+      console.log("disconnected ", stream.id);
+      connections = connections.filter((s) => s !== stream);
+    });
+  }
+});
+
 server.on("request", async (req, res) => {
   const path = req.headers[":path"];
   const method = req.headers[":method"];
@@ -53,11 +73,17 @@ server.on("request", async (req, res) => {
     const data = Buffer.concat(buffers).toString();
     const { user, text } = JSON.parse(data);
 
-    /*
-     *
-     * some code goes here
-     *
-     */
+    msg.push({
+      user,
+      text,
+      time: Date.now(),
+    });
+
+    res.end();
+
+    connections.forEach((stream) => {
+      stream.write(JSON.stringify({ msg: getMsgs() }));
+    });
   }
 });
 
